e3ff139af9658e6938b8eb660031fdb6c4e40b45
Mukilan <mukilannarayanamoorthy@gmail.com>
2017-11-12 14:11:15 +0530

    Final Commit-Working Fine

---
+++ Surfer Vio/Surfer Vio/Mailer.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Mailer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RadLabel1 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadButton1 = New Telerik.WinControls.UI.RadButton()
+        Me.txtEmail = New Telerik.WinControls.UI.RadTextBox()
+        Me.txtToEmail = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadLabel2 = New Telerik.WinControls.UI.RadLabel()
+        Me.txtPassword = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadLabel3 = New Telerik.WinControls.UI.RadLabel()
+        Me.txtSubject = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadLabel4 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel5 = New Telerik.WinControls.UI.RadLabel()
+        Me.txtBody = New System.Windows.Forms.TextBox()
+        Me.TelerikMetroBlueTheme1 = New Telerik.WinControls.Themes.TelerikMetroBlueTheme()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtEmail, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtToEmail, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtPassword, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtSubject, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RadLabel1
+        '
+        Me.RadLabel1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel1.Location = New System.Drawing.Point(12, 37)
+        Me.RadLabel1.Name = "RadLabel1"
+        Me.RadLabel1.Size = New System.Drawing.Size(49, 25)
+        Me.RadLabel1.TabIndex = 0
+        Me.RadLabel1.Text = "From:"
+        '
+        'RadButton1
+        '
+        Me.RadButton1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadButton1.Location = New System.Drawing.Point(12, 445)
+        Me.RadButton1.Name = "RadButton1"
+        Me.RadButton1.Size = New System.Drawing.Size(388, 68)
+        Me.RadButton1.TabIndex = 0
+        Me.RadButton1.Text = "Send"
+        Me.RadButton1.ThemeName = "TelerikMetroBlue"
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.txtEmail.Location = New System.Drawing.Point(133, 37)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(267, 27)
+        Me.txtEmail.TabIndex = 0
+        Me.txtEmail.ThemeName = "Windows8"
+        '
+        'txtToEmail
+        '
+        Me.txtToEmail.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.txtToEmail.Location = New System.Drawing.Point(133, 70)
+        Me.txtToEmail.Name = "txtToEmail"
+        Me.txtToEmail.Size = New System.Drawing.Size(267, 27)
+        Me.txtToEmail.TabIndex = 1
+        Me.txtToEmail.ThemeName = "Windows8"
+        '
+        'RadLabel2
+        '
+        Me.RadLabel2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel2.Location = New System.Drawing.Point(12, 70)
+        Me.RadLabel2.Name = "RadLabel2"
+        Me.RadLabel2.Size = New System.Drawing.Size(30, 25)
+        Me.RadLabel2.TabIndex = 2
+        Me.RadLabel2.Text = "To:"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.txtPassword.Location = New System.Drawing.Point(133, 103)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(267, 27)
+        Me.txtPassword.TabIndex = 3
+        Me.txtPassword.ThemeName = "Windows8"
+        '
+        'RadLabel3
+        '
+        Me.RadLabel3.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel3.Location = New System.Drawing.Point(12, 103)
+        Me.RadLabel3.Name = "RadLabel3"
+        Me.RadLabel3.Size = New System.Drawing.Size(80, 25)
+        Me.RadLabel3.TabIndex = 4
+        Me.RadLabel3.Text = "Password:"
+        '
+        'txtSubject
+        '
+        Me.txtSubject.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.txtSubject.Location = New System.Drawing.Point(133, 136)
+        Me.txtSubject.Name = "txtSubject"
+        Me.txtSubject.Size = New System.Drawing.Size(267, 27)
+        Me.txtSubject.TabIndex = 5
+        Me.txtSubject.ThemeName = "Windows8"
+        '
+        'RadLabel4
+        '
+        Me.RadLabel4.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel4.Location = New System.Drawing.Point(12, 136)
+        Me.RadLabel4.Name = "RadLabel4"
+        Me.RadLabel4.Size = New System.Drawing.Size(65, 25)
+        Me.RadLabel4.TabIndex = 6
+        Me.RadLabel4.Text = "Subject:"
+        '
+        'RadLabel5
+        '
+        Me.RadLabel5.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold)
+        Me.RadLabel5.Location = New System.Drawing.Point(12, 167)
+        Me.RadLabel5.Name = "RadLabel5"
+        Me.RadLabel5.Size = New System.Drawing.Size(57, 25)
+        Me.RadLabel5.TabIndex = 7
+        Me.RadLabel5.Text = "Body :"
+        '
+        'txtBody
+        '
+        Me.txtBody.Location = New System.Drawing.Point(12, 193)
+        Me.txtBody.Multiline = True
+        Me.txtBody.Name = "txtBody"
+        Me.txtBody.Size = New System.Drawing.Size(388, 246)
+        Me.txtBody.TabIndex = 8
+        '
+        'Mailer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(412, 525)
+        Me.Controls.Add(Me.txtBody)
+        Me.Controls.Add(Me.RadLabel5)
+        Me.Controls.Add(Me.txtSubject)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.RadLabel4)
+        Me.Controls.Add(Me.txtToEmail)
+        Me.Controls.Add(Me.RadLabel3)
+        Me.Controls.Add(Me.RadLabel2)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.RadButton1)
+        Me.Controls.Add(Me.RadLabel1)
+        Me.Name = "Mailer"
+        Me.Text = "Express Mail Client"
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadButton1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtEmail, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtToEmail, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtPassword, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtSubject, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents RadLabel1 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadButton1 As Telerik.WinControls.UI.RadButton
+    Friend WithEvents txtEmail As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents txtToEmail As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadLabel2 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents txtPassword As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadLabel3 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents txtSubject As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadLabel4 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel5 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents txtBody As System.Windows.Forms.TextBox
+    Friend WithEvents TelerikMetroBlueTheme1 As Telerik.WinControls.Themes.TelerikMetroBlueTheme
+End Class
---
+++ Surfer Vio/Surfer Vio/Mailer.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Net.Mail
+
+Public Class Mailer
+
+    Private Sub RadButton1_Click(sender As Object, e As EventArgs) Handles RadButton1.Click
+        Try
+            Dim MyMailMessage As New MailMessage()
+            MyMailMessage.From = New MailAddress(txtEmail.Text)
+            MyMailMessage.To.Add(txtToEmail.Text)
+            MyMailMessage.Subject = txtSubject.Text
+            MyMailMessage.Body = txtBody.Text
+            Dim SMTPServer As New SmtpClient("smtp.gmail.com")
+            SMTPServer.Port = 587
+            SMTPServer.Credentials = New System.Net.NetworkCredential(txtEmail.Text, txtPassword.Text)
+            SMTPServer.EnableSsl = True
+            SMTPServer.Send(MyMailMessage)
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
